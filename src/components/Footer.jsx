85e99ec1c68d7d2c687e67a430cbecff7a22a223
Gabriel <jgabrieldasilva2020@gmail.com>
2025-11-01 09:48:44 -0300

    Estilizando o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Footer = () =>{
     return(
-        <p>
-            ®2025 CEST - Todos os Direitos Reservados
-        </p>
+        <div className="text-center py-10 bg-[#000066] mt-20">
+            <p className=" text-white ">
+                ®2025 CEST - Todos os Direitos Reservados
+            </p>
+        </div>
     )
 }
 
